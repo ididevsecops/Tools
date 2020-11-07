4c50de276166d67381d3c3c23fa8fb136fb5c487
sec1ididevsecops <67468667+sec1ididevsecops@users.noreply.github.com>
2020-11-07 08:10:23 -0500

    Simplified Pester Test

--- pester.ps1
+++ pester.ps1
@@ -1,46 +1,16 @@
-Describe "Our Application" {
-    #region Azure App Service Endpoint
-    Context "When Stopped" -Tag "Offline", "App Service" {
-        It "Azure App Service Endpoint Should Not Respond" {
-            { Invoke-WebRequest -Uri "https://as-webapp-dev-idso-001.azurewebsites.net" -Method Get -SkipHttpErrorCheck } | Should -Throw
-        }
-    }
-    Context "When Started" -Tag "Online", "App Service" {
-        It "Azure App Service Endpoint Should Return Status Code 403" {
-            (Invoke-WebRequest -Uri "https://as-webapp-dev-idso-001.azurewebsites.net" -Method Get -SkipHttpErrorCheck).StatusCode | Should -Be 403
-        }
-    }
-    #endregion Azure App Service Endpoint
-
-    #region Azure Front Door Endpoint
-    Context "When Stopped" -Tag "Offline", "App Service" {
-        It "Azure Front Door Should Not Respond" {
-            { Invoke-WebRequest -Uri "https://fd-shared-dev-idso-000.azurefd.net" -Method Get -SkipHttpErrorCheck } | Should -Throw
-        }
-    }
-    Context "When Started" -Tag "Online", "Front Door" {
-        It "Azure Front Door Endpoint Should Return Status Code 200" {
-            (Invoke-WebRequest -Uri "https://fd-shared-dev-idso-000.azurefd.net" -Method Get -SkipHttpErrorCheck).StatusCode | Should -Be 200
-        }
-        It "Azure Front Door Endpoint Should Return 0 Bytes" {
-            (Invoke-WebRequest -Uri "https://fd-shared-dev-idso-000.azurefd.net" -Method Get -SkipHttpErrorCheck).RawContentLength | Should -Be 0
-        }
-    }
-    #endregion Azure Front Door Endpoint
-
-    #region Custom Domain Endpoint
-    Context "When Stopped" -Tag "Offline" {
-        It "Public Endpoint Should Not Respond" {
-            { Invoke-WebRequest -Uri "https://api.ididevsecops.net" -Method Get -SkipHttpErrorCheck } | Should -Throw
-        }
-    }
+Describe "The Application" {
     Context "When Started" -Tag "Online" {
-        It "Custom Domain Endpoint Should Return Status Code 200" {
-            (Invoke-WebRequest -Uri "https://api.ididevsecops.net" -Method Get -SkipHttpErrorCheck).StatusCode | Should -Be 200
+        BeforeAll {
+            function GetUri () { "http://app.ididevsecops.net" }
+            function GetHeader() { "WebApp1" }
+            function InvokeUri () { Invoke-WebRequest -Uri $(GetUri) -Method Get -SkipHttpErrorCheck -SkipCertificateCheck }
         }
-        It "Custom Domain Endpoint Should Endpoint Return 0 Bytes" {
-            (Invoke-WebRequest -Uri "https://api.ididevsecops.net" -Method Get -SkipHttpErrorCheck).RawContentLength | Should -Be 0
+        It "Should Respond" {
+            $Response = $(InvokeUri)
+            $Response.StatusCode | Should -Be 200
+            $Response.RawContentLength | Should -Be 0
+            $Response.Headers["idso"] | Should -Not -BeNullOrEmpty
+            $Response.Headers["idso"] | Should -Be $(GetHeader)
         }
     }
-    #endregion Custom Domain Endpoint
 }
