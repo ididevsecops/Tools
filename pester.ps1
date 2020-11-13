ce77ab7fd072972118ed1a282595ad3a73af25a4
sec1ididevsecops <67468667+sec1ididevsecops@users.noreply.github.com>
2020-11-13 13:42:18 -0500

    Updated Pester.ps1 with AfterAll.

--- pester.ps1
+++ pester.ps1
@@ -1,17 +1,14 @@
 Describe "The Application" {
     Context "When Started" -Tag "Online" {
         BeforeAll {
-            # TODO - Parameterize after Pester 5.1.0
-            $Uri = "https://localhost:443/" # IIS
-            # $Uri = "https://localhost:8443/" # Kestrel
-            # $Uri = "https://app-mcguffin-prod-idso-000.azurewebsites.net:443/" # App Service East US
-            # $Uri = "https://app-mcguffin-prod-idso-001.azurewebsites.net:443/" # App Service North Europe
-            # $Uri = "https://fd-shared-prod-idso-000.azurefd.net:443/" # Front Door
-            # $Uri = "https://mcguffin.ididevsecops.net:443/" # Public
+            # $Uri = "https://localhost:443/" # IIS (Development)
+            # $Uri = "https://localhost:8443/" # Kestrel (Development)
+            # $Uri = "https://app-mcguffin-prod-idso-000.azurewebsites.net:443/" # Azure App Service (East US)
+            # $Uri = "https://app-mcguffin-prod-idso-001.azurewebsites.net:443/" # Azure App Service (North Europe)
+            $Uri = "https://fd-shared-prod-idso-000.azurefd.net:443/" # Azure Front Door
+            # $Uri = "https://mcguffin.ididevsecops.net:443/" # Public Domain
 
             $Response = Invoke-WebRequest -Uri $Uri -Method Get -SkipHttpErrorCheck -SkipCertificateCheck
-
-            Write-Host $Response.Headers["idso"] -ForegroundColor Blue
         }
         It "Should Return 200" {
             $Response.StatusCode | Should -Be 200
@@ -21,6 +18,9 @@ Describe "The Application" {
         }
         It "Should Return Header" {
             $Response.Headers["idso"] | Should -Not -BeNullOrEmpty
+        }
+        AfterAll {
+            Write-Host $Response.Headers["idso"] -ForegroundColor DarkYellow
         }
     }
 }
