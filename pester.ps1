6b05b55486bcaf410731d2b30ecf1d3b766b47aa
sec1ididevsecops <67468667+sec1ididevsecops@users.noreply.github.com>
2020-11-18 09:21:37 -0500

    More Pester test changes.

--- pester.ps1
+++ pester.ps1
@@ -2,7 +2,7 @@ Describe "The Application" {
     Context "When Started" -Tag "Online" {
         BeforeAll {
             # IIS (Development)
-            $Uri = "http://localhost:80/" 
+            $Uri = "http://localhost:80/"
             # $Uri = "https://localhost:443/"
 
             # Kestrel (Development)
@@ -21,26 +21,47 @@ Describe "The Application" {
             # $Uri = "http://fd-shared-prod-idso-000.azurefd.net:80/"
             # $Uri = "https://fd-shared-prod-idso-000.azurefd.net:443/"
 
-            # Azure Virtual Machine (vmmcguffin000, pipvmmcguffin000prodidso000.eastus.cloudapp.azure.com)
+            # Azure Virtual Machine Public IP (vmmcguffin000, pipvmmcguffin000prodidso000.eastus.cloudapp.azure.com)
             # $Uri = "http://104.45.196.133:80/"
             # $Uri = "https://104.45.196.133:443/"
 
-            # Azure Virtual Machine (vmmcguffin000, pipvmmcguffin000prodidso000.eastus.cloudapp.azure.com)
+            # Azure Virtual Machine Public DNS (vmmcguffin000, pipvmmcguffin000prodidso000.eastus.cloudapp.azure.com)
             # $Uri = "http://pipvmmcguffin000prodidso000.eastus.cloudapp.azure.com:80/"
             # $Uri = "https://pipvmmcguffin000prodidso000.eastus.cloudapp.azure.com:443/"
-            
-            # Azure Load Balancer for Virtual Machine(s) (vmmcguffin*)
+
+            # Azure Virtual Machine Public IP (vmmcguffin001, pipvmmcguffin000prodidso001.eastus.cloudapp.azure.com)
+            # $Uri = ""
+            # $Uri = ""
+
+            # Azure Virtual Machine Public DNS (vmmcguffin001, pipvmmcguffin001prodidso000.eastus.cloudapp.azure.com)
+            # $Uri = ""
+            # $Uri = ""
+
+            # Azure Load Balancer Public IP for Virtual Machine(s) (vmmcguffin*)
             # $Uri = "http://23.96.110.193:80/"
             # $Uri = "https://23.96.110.193:443/"
 
-            # Azure Load Balancer for Virtual Machine(s) (vmmcguffin*)
+            # Azure Load Balancer Public DNS for Virtual Machine(s) (vmmcguffin*)
+            # $Uri = "http://pipvmmcguffin001prodidso000.eastus.cloudapp.azure.com:80/"
+            # $Uri = "https://pipvmmcguffin001prodidso000.eastus.cloudapp.azure.com:443/"
+
+            # Azure Load Balancer Public IP for Virtual Machine(s) (vmmcguffin*)
+            # $Uri = "http://23.96.110.193:80/"
+            # $Uri = "https://23.96.110.193:443/"
+
+            # Azure Load Balancer Public DNS for Virtual Machine(s) (vmmcguffin*)
             # $Uri = "http://pipvmmcguffinprodidso000.eastus.cloudapp.azure.com:80/"
             # $Uri = "https://pipvmmcguffinprodidso000.eastus.cloudapp.azure.com:443/"
 
-            # $Uri = "http://mcguffin.ididevsecops.net:80/" # Public Domain
-            # $Uri = "https://mcguffin.ididevsecops.net:443/" # Public Domain
+            # Custom DNS
+            # $Uri = "http://mcguffin.ididevsecops.net:80/"
+            # $Uri = "https://mcguffin.ididevsecops.net:443/"
+            # $Uri = "https://mcguffin.ididevsecops.net:443/"
 
             $Response = Invoke-WebRequest -Uri $Uri -Method Get -SkipHttpErrorCheck -SkipCertificateCheck
+
+            # Quell PSScriptAnalyzer warning about a variable is assigned but never used.
+            $Response | Out-Null
         }
         It "Should Return 200" {
             $Response.StatusCode | Should -Be 200
@@ -52,7 +73,8 @@ Describe "The Application" {
             $Response.Headers["idso"] | Should -Not -BeNullOrEmpty
         }
         AfterAll {
-            Write-Host $Response.Headers["idso"] -ForegroundColor DarkYellow
+            Write-Host "Uri Tested = $Uri" -ForegroundColor DarkYellow
+            Write-Host "Header Returned = $($Response.Headers['idso'])" -ForegroundColor DarkYellow
         }
     }
 }
